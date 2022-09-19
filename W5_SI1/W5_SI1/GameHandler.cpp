56fd174f7e808c4baa0d5eaa88b17c207bd07c1e
Marcin Salamon <marcin.salamon@motorolasolutions.com>
2022-09-19 12:05:01 +0200

    add win condition and tie condition

--- W5_SI1/W5_SI1/GameHandler.cpp
+++ W5_SI1/W5_SI1/GameHandler.cpp
@@ -24,11 +24,14 @@ void GameHandler::runGame()
 	else if (player1.sign == 'O')
 		player2.sign = 'X';
 
-
-	while (true) {
-		makePlayerMove(player1);
-		makePlayerMove(player2);
-	}
+	int result{};
+	do {
+		result = makePlayerMove(player1);
+		if (result == 1) break;
+		result = makePlayerMove(player2);
+		counterOfTurn++;
+		if (counterOfTurn == board.getBoardSize().first * board.getBoardSize().second) { std::cout << "Remis"; break; }
+	} while (result != 1);
 }
 
 bool GameHandler::checkIsValidMove(std::pair<int, int> playerMove)
@@ -57,7 +60,7 @@ bool GameHandler::isOccupied(std::pair<int, int>playerMove)
 	return !isFree;
 }
 
-void GameHandler::makePlayerMove(Player player)
+int GameHandler::makePlayerMove(Player player)
 {
 	std::cout << "Turn:    " << player.name << " " << player.sign << std::endl;
 	board.printBoard();
@@ -65,18 +68,17 @@ void GameHandler::makePlayerMove(Player player)
 	while (!checkIsValidMove(playerMove = player.inputCoordinate()));
 	while (isOccupied(playerMove)) { std::cout << "Field is occupied" << std::endl; break; }
 	upadateBoard(playerMove, player);
-	if (isHorizontal(playerMove) || isVertical(playerMove)) { std::cout << "wygrales: " << player.name << std::endl; system("pause"); }
-	//system("cls");
-
-}
-
-bool GameHandler::isInBoardRange(int x, int y)
-{
-	if (x >= 0 && x < board.getBoardSize().first && y >= 0 && y < board.getBoardSize().second) {
-		return true;
+	if (isWin(playerMove)) {
+		std::cout << "Wygrales: " << player.name << std::endl; system("pause");
+		return 1;
 	}
-	return false;
+	else
+		return 0;
+
+	system("cls");
+
 }
+
 
 bool GameHandler::isVertical(std::pair<int, int> playerMove)
 {
@@ -87,11 +89,6 @@ bool GameHandler::isVertical(std::pair<int, int> playerMove)
 	//vertical
 	for (int yPos{ 0 }; yPos < ySize - 4;yPos++) {
 
-		//std::cout << "(*board.getBoard())[xPos][y]: " << (*board.getBoard())[x][yPos] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 1]:" << (*board.getBoard())[x][yPos +1] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 2]:" << (*board.getBoard())[x][yPos + 2] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 3]:" << (*board.getBoard())[x][yPos +3] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 4]:" << (*board.getBoard())[x][yPos + 4] << std::endl;
 		if ((*board.getBoard())[x][y] == (*board.getBoard())[x][yPos] &&
 			(*board.getBoard())[x][yPos] == (*board.getBoard())[x][yPos + 1] &&
 			(*board.getBoard())[x][yPos + 1] == (*board.getBoard())[x][yPos + 2] &&
@@ -113,11 +110,6 @@ bool GameHandler::isHorizontal(std::pair<int, int> playerMove)
 	//horizontal
 	for (int xPos{ 0 }; xPos < xSize - 4;xPos++) {
 
-		//std::cout << "(*board.getBoard())[xPos][y]: " << (*board.getBoard())[xPos][y]  << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 1]:" << (*board.getBoard())[xPos + 1][y] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 2]:" << (*board.getBoard())[xPos + 2][y] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 3]:" << (*board.getBoard())[xPos + 3][y] << std::endl;
-		//std::cout << "(*board.getBoard())[xPos + 4]:" << (*board.getBoard())[xPos + 4][y] << std::endl;
 		if ((*board.getBoard())[x][y] == (*board.getBoard())[xPos][y] &&
 			(*board.getBoard())[xPos][y] == (*board.getBoard())[xPos + 1][y] &&
 			(*board.getBoard())[xPos + 1][y] == (*board.getBoard())[xPos + 2][y] &&
@@ -129,12 +121,75 @@ bool GameHandler::isHorizontal(std::pair<int, int> playerMove)
 	return false;
 }
 
-bool GameHandler::isCross(std::pair<int, int> playerMove)
+bool GameHandler::isCrossBackslash(std::pair<int, int> playerMove)
 {
-	return false;
+	auto x = playerMove.first;
+	auto y = playerMove.second;
+	int xSize = board.getBoardSize().first;
+	int ySize = board.getBoardSize().second;
+
+
+
+	bool xCrossBackslash{ false };
+	for (int i{ 0 }; i < ySize - 4; i++) {
+		for (int xPos{ 0 }, yPos{ 0 }; yPos+i < ySize - 4; xPos++, yPos++) {
+			if ((*board.getBoard())[x][y] == (*board.getBoard())[xPos][yPos+i] &&
+				(*board.getBoard())[xPos][yPos+i] == (*board.getBoard())[xPos + 1][yPos+i + 1] &&
+				(*board.getBoard())[xPos + 1][yPos+i + 1] == (*board.getBoard())[xPos + 2][yPos + i + 2] &&
+				(*board.getBoard())[xPos + 2][yPos+i + 2] == (*board.getBoard())[xPos + 3][yPos+i + 3] &&
+				(*board.getBoard())[xPos + 3][yPos+i + 3] == (*board.getBoard())[xPos + 4][yPos+i + 4]) {
+				return xCrossBackslash = true;
+			}
+		}
+	}
+
+	bool yCrossBackslash{ false };
+	for (int i{ 0 }; i < xSize - 4; i++) {
+		for (int xPos{ 0 }, yPos{ 0 }; xPos + i < xSize - 4; xPos++, yPos++) {
+			if ((*board.getBoard())[x][y] == (*board.getBoard())[xPos+i][yPos] &&
+				(*board.getBoard())[xPos+i][yPos] == (*board.getBoard())[xPos + i+1][yPos + 1] &&
+				(*board.getBoard())[xPos +i+ 1][yPos + 1] == (*board.getBoard())[xPos + i + 2][yPos + 2] &&
+				(*board.getBoard())[xPos + i + 2][yPos+ 2] == (*board.getBoard())[xPos + i + 3][yPos + 3] &&
+				(*board.getBoard())[xPos + i + 3][yPos + 3] == (*board.getBoard())[xPos + i + 4][yPos + 4]) {
+				return yCrossBackslash = true;
+			}
+		}
+	}
+
+
+	bool yCrossSlash{ false };
+	for (int i{ 0 }; i < ySize - 4; i++) {
+		for (int xPos{ xSize - 1 }, yPos{ 0 }; yPos + i< ySize - 4; xPos--, yPos++) {
+			if ((*board.getBoard())[x][y] == (*board.getBoard())[xPos][yPos+i] &&
+				(*board.getBoard())[xPos][yPos+i] == (*board.getBoard())[xPos - 1][yPos+i + 1] &&
+				(*board.getBoard())[xPos - 1][yPos + i + 1] == (*board.getBoard())[xPos - 2][yPos + i + 2] &&
+				(*board.getBoard())[xPos - 2][yPos + i + 2] == (*board.getBoard())[xPos - 3][yPos + i + 3] &&
+				(*board.getBoard())[xPos - 3][yPos + i + 3] == (*board.getBoard())[xPos - 4][yPos + i + 4]) {
+				yCrossSlash = true;
+			}
+		}
+	}
+
+	bool xCrossSlash{ false };
+	for (int i{ 0 }; i < ySize - 4; i++) {
+		for (int xPos{ xSize - 1 }, yPos{ 0 }; xPos - i > 4; xPos--, yPos++) {
+			if ((*board.getBoard())[x][y] == (*board.getBoard())[xPos -i][yPos] &&
+				(*board.getBoard())[xPos - i][yPos] == (*board.getBoard())[xPos - 1 - i][yPos + 1] &&
+				(*board.getBoard())[xPos - 1 - i][yPos + 1] == (*board.getBoard())[xPos - 2 - i][yPos + 2] &&
+				(*board.getBoard())[xPos - 2 - i][yPos + 2] == (*board.getBoard())[xPos - 3 - i][yPos + 3] &&
+				(*board.getBoard())[xPos - 3 - i][yPos + 3] == (*board.getBoard())[xPos - 4 - i][yPos+ 4]) {
+				xCrossSlash = true;
+			}
+		}
+	}
+
+	if (xCrossBackslash || yCrossBackslash || yCrossSlash || xCrossSlash) { return true; }
+	else return false;
 }
 
 
-bool GameHandler::isWin() {
-	return 1;
+bool GameHandler::isWin(std::pair<int, int>playerMove) {
+
+	if (isVertical(playerMove) || isHorizontal(playerMove) || isCrossBackslash(playerMove)) { return true; }
+	else return false;
 }
--- W5_SI1/W5_SI1/GameHandler.h
+++ W5_SI1/W5_SI1/GameHandler.h
@@ -8,6 +8,7 @@ class GameHandler
 	Player player1;
 	Player player2;
 	Board board;
+	int counterOfTurn{ 0 };
 
 
 public:
@@ -17,11 +18,10 @@ public:
 	bool checkIsValidMove(std::pair<int, int>);
 	void upadateBoard(std::pair<int, int>playerMove, Player player);
 	bool isOccupied(std::pair<int, int>playerMove);
-	void makePlayerMove(Player player);
-	bool isInBoardRange(int x, int y);
+	int makePlayerMove(Player player);
 	bool isVertical(std::pair<int, int>playerMove);
 	bool isHorizontal(std::pair<int, int>playerMove);
-	bool isCross(std::pair<int, int>playerMove);
-	bool isWin();
+	bool isCrossBackslash(std::pair<int, int>playerMove);
+	bool isWin(std::pair<int, int>playerMove);
 };
 
